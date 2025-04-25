1f14c3aa7cc64c5e5351d8977ea1e7369f9372d2
Cristhian Cintra <64995266+CintraB@users.noreply.github.com>
2025-04-25 14:10:11 -0300

    Create loggers.js

---
+++ src/loggers.js
@@ -0,0 +1,34 @@
+const { createLogger, format, transports } = require('winston');
+require('dotenv').config();
+const path = require('path');
+
+// Formato personalizado para exibir logs no console
+const logFormat = format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.printf(({ timestamp, level, message }) => `${timestamp} [${level.toUpperCase()}]: ${message}`)
+);
+
+const logDir = path.resolve(__dirname, '../logs');
+
+const logger = createLogger({
+    level: 'debug', // Captura todos os níveis a partir de debug
+    format: logFormat,
+    transports: [
+        new transports.Console({ level: 'debug' }), // Console captura todos os níveis
+        new transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' }),
+        //new transports.File({ filename: 'logs/error.log', level: 'error' }), // Apenas erros
+        new transports.File({ filename: path.join(logDir, 'combined.log') })
+        //new transports.File({ filename: 'logs/combined.log' }) // Captura todos os níveis sem restrição
+    ]
+});
+
+// Desativar o log no console para produção
+if (process.env.NODE_ENV === 'production') {
+    logger.transports.forEach((t) => {
+        if (t instanceof transports.Console) {
+            logger.remove(t);
+        }
+    });
+}
+
+module.exports = logger;
